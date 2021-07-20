66fa6d8911097c9ae3695a56f851560473cfddb1
Roy <roybett93@gmail.com>
2021-07-19 23:22:12 -0400

    fixed leaked api key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: "ab23561a4ee754b3ebac52f163bbae84",
+    apiKey: "API key goes here",
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
